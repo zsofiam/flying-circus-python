7fa608d67465510f8cff03f134732021ad70c973
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-02-10 15:32:17 +0100

    create login form

--- app.py
+++ app.py
@@ -1,3 +1,61 @@
+from flask import Flask, session, redirect, url_for, escape, request, render_template
 import data
 
-# put your code here
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/')
+def index():
+    if 'email' in session and 'password' in session and session['email'] in data.users \
+            and data.verify_password(session['password'], data.users[session['email']]):
+        return 'Logged in as %s <br/><br/><a href="/logout">Logout</a>' % escape(session['email'])
+    #return 'You are not logged in'
+    return redirect('/login')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['email'] = request.form['email']
+        session['password'] = request.form['password']
+        password = session['password']
+        if 'email' in session and 'password' in session and session['email'] in data.users\
+                and data.verify_password(password, data.users[session['email']]):
+            return redirect("/")
+        else:
+            return '''Wrong password!
+            <br/><br/>
+            <a href="/logout">Back to main page</a>'''
+    return '''
+        <form method="post">
+            <label for="email">Email:</label>
+            <p><input type=email id="email" name=email>
+            <br/><br/>
+            <label for="password">Password:</label>
+            <p><input type=password id="password" name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('email', None)
+    session.pop('password', None)
+    return redirect(url_for('index'))
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         data_manager.create_comment(request.form.get('name'), request.form.get('text'))
+#
+#     comments = data_manager.get_comments()
+#     return render_template('index.html', comments=comments)
+
+
+if __name__ == '__main__':
+    app.run()
--- data.py
+++ data.py
@@ -1,5 +1,13 @@
 # TODO: add your users and hashed passwords here
-users = {'john@doe.com': '$2b$12$/TYFvXOy9wDQUOn5SKgTzedwiqB6cm.UIfPewBnz0kUQeK9Eu4mSC'}
+import bcrypt
+
+
+users = {'john@doe.com': '$2b$12$/TYFvXOy9wDQUOn5SKgTzedwiqB6cm.UIfPewBnz0kUQeK9Eu4mSC',
+         'abc@gmail.com': '$2b$12$rqj0oZHzTuGh8fSJXFbrLOEMG6VQkCud4uNcD1y/bqN5Tm0Lc5EP.',
+         'def@gmail.com': '$2b$12$h8Dk54t93hoHMrcMcerEauDjxHUU4qU9aVxUbk9IJpFPgtGPsaQ9u',
+         'ghi@gmail.com': '2b$12$aOS/s.kqcn1CzZnUaIHNfeDYPWwlQJfqsO3mH/F5JEa59QMbG0eL2',
+         'secure@gmail.com': '$2b$12$PYVmkHz8gC0Srs96jUlvHeK4kT.l4vSem17TTmPaqC/nY0p4pUeDu'}
+
 
 questions = {
     "I ______ bus on Mondays.": {
@@ -33,3 +41,31 @@ questions = {
         "d. do your sister work": False
     }
 }
+
+
+def hash_password(plain_text_password):
+    # By using bcrypt, the salt is saved into the hash itself
+    hashed_bytes = bcrypt.hashpw(plain_text_password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_bytes.decode('utf-8')
+
+
+def verify_password(plain_text_password, hashed_password):
+    hashed_bytes_password = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(plain_text_password.encode('utf-8'), hashed_bytes_password)
+
+
+if __name__ == '__main__':
+    # Test the above functions manually
+    original_password = 'my_very_secureP4ssword!'  # From registration form
+    print('original_password: ' + original_password)
+
+    hashed_password = hash_password(original_password)  # This shall be saved in the DB
+    print('hashed_password: ' + hashed_password)
+
+    user_input_password = 'Hey Siri, what is my password?'  # From a login form, a mistyped input
+    is_matching = verify_password(user_input_password, hashed_password)
+    print('is_matching: ' + str(is_matching))
+
+    user_input_password = 'my_very_secureP4ssword!'  # From a login form, the correct input
+    is_matching = verify_password(user_input_password, hashed_password)
+    print('is_matching: ' + str(is_matching))
